76368e3c0830ae548640fb2ee71e1242159f1cb5
US\anukota <anukota@deloitte.com>
2019-03-18 11:16:23 -0400

    Base code

---
+++ docker-springboot-example/src/main/java/com/docker/example/dockerspringbootexample/DockerSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.docker.example.dockerspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringbootExampleApplication.class, args);
+	}
+
+}
